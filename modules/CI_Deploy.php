d703ec37d2606cf4e37348a3696343d45e1c617a
Eugene Savchenko <savchenko.eo@gmail.com>
2016-03-30 12:56:09 +0300

    Remove .idea folder. Added Deployer for CodeIgniter

---
+++ modules/CI_Deploy.php
@@ -0,0 +1,137 @@
+<?php if ( ! defined( 'BASEPATH' ) ) exit( 'No direct script access allowed' );
+
+/**
+ * Class CI_Deploy
+ *
+ * Deploying project with special configuration for CodeIgniter
+ *
+ * @class CI_Deploy
+ *
+ * @extends PhpDeploy
+ */
+final class CI_Deploy extends PhpDeploy
+{
+
+    /**
+     * Migrations pat in CodeIgniter
+     *
+     * @const
+     */
+    const MIGRATIONS_PATH = PhpDeploy::DOC_ROOT_PATH . '/application/migrations';
+
+
+    /**
+     * Current migration index file
+     *
+     * @const
+     */
+    const MIGRATION_INDEX_FILE = 'var/migration_index';
+
+
+    /**
+     * Configs path, which will be copied to CI
+     *
+     * @const
+     */
+    const PRODUCTION_CONFIGS_PATH = 'configs';
+
+
+    /**
+     * CodeIgniter production configs path
+     *
+     * @const
+     */
+    const CI_PRODUCTION_CONFIGS_PATH = PhpDeploy::DOC_ROOT_PATH . '/application/config/production';
+
+
+    /**
+     * CI_Deploy constructor.
+     *
+     * @method __construct
+     * @access public
+     */
+    public function __construct( )
+    {
+
+        parent::__construct( );
+
+        $this->_load();
+
+    }
+
+
+    /**
+     * Do deploy to server
+     *
+     * @method do_deploy
+     * @access public
+     *
+     * @return string
+     */
+    public function do_deploy()
+    {
+
+        $rs = parent::do_deploy();
+
+        ob_start();
+
+        echo $this->_copy_ci_configs();
+        echo $this->_run_migration_and_update_index();
+
+        $rs .= ob_get_clean();
+
+        return $rs;
+
+    }
+
+
+    /**
+     * Load files and folders.
+     * Create its, if not found.
+     *
+     * @method _load
+     * @access private
+     */
+    private function _load( )
+    {
+
+        // Check migration index file exists
+        $this->_create_file_if_not_exists( CI_Deploy::MIGRATION_INDEX_FILE );
+
+    }
+
+
+    /**
+     * Copy production configs to CodeIgniter
+     *
+     * @method _copy_ci_configs
+     * @access private
+     *
+     * @return string
+     */
+    private function _copy_ci_configs( )
+    {
+        
+        $cmd = 'cp -Rf ' . CI_Deploy::PRODUCTION_CONFIGS_PATH . '/. ' . CI_Deploy::CI_PRODUCTION_CONFIGS_PATH . ';';
+        
+        $cmd .= 'find ' . CI_Deploy::CI_PRODUCTION_CONFIGS_PATH . ' -type f | xargs chmod -v 600;';
+        
+        return shell_exec( $cmd );
+        
+    }
+
+
+    /**
+     * Run migration and update index
+     *
+     * @method _run_migration_and_update_index
+     * @access private
+     *
+     * @return string
+     */
+    private function _run_migration_and_update_index( )
+    {
+        return 'NaN';
+    }
+
+}

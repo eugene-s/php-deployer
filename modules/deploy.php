6e1395458eacab1385ec5bc812b32e1ffbc74567
Eugene Savchenko <savchenko.eo@gmail.com>
2016-03-31 20:34:21 +0300

    Fix

--- modules/deploy.php
+++ modules/deploy.php
@@ -290,6 +290,9 @@ class PhpDeploy extends BaseDeploy
 
         $cmd[] = 'cp -Rf ' . PhpDeploy::TMP_PATH . '/. ' . $this->_build_folder;
 
+        $cmd[] = "find {$this->_build_folder} -type f | xargs chmod -v 644";
+        $cmd[] = "find {$this->_build_folder} -type d | xargs chmod -v 755";
+
         // Remove unnecessary folders
         foreach ( CLEAN_FOLDERS as $folder_name ) {
             $cmd[] = "find {$this->_build_folder} -type d -name $folder_name -exec rm -rf {} +";
@@ -319,9 +322,6 @@ class PhpDeploy extends BaseDeploy
         $cmd[] = 'rm ' . PhpDeploy::DOC_ROOT_PATH;
 
         $cmd[] = 'ln -s ' . $this->_build_folder . ' ' . PhpDeploy::DOC_ROOT_PATH;
-
-        $cmd[] = 'find ' . PhpDeploy::DOC_ROOT_PATH . ' -type f | xargs chmod -v 644';
-        $cmd[] = 'find ' . PhpDeploy::DOC_ROOT_PATH . ' -type d | xargs chmod -v 755';
 
         return $cmd;
 
